9d369efbeef196450348a6b7eb2c17c3c370698d
alejolesor <castiblancosolerd@mail.com>
2022-01-20 09:43:59 -0500

    db

--- database/schema.sql
+++ database/schema.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS `BeerItem`
 (
     `id` bigint NOT NULL AUTO_INCREMENT,
-    `name` varchar(100) NOT NULL,
-    `brewery` varchar(100) DEFAULT NULL,
-    `country` varchar(100) DEFAULT NULL,
+    `name` varchar(50) NOT NULL,
+    `brewery` varchar(60) DEFAULT NULL,
+    `country` varchar(50) DEFAULT NULL,
     `price` float DEFAULT NULL,
-    `currency` varchar(100) DEFAULT NULL,
+    `currency` varchar(50) DEFAULT NULL,
     PRIMARY KEY (`id`)
 )
